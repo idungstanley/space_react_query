42227ab3941b68aa49c174164a51f45e97a301aa
devransom-github <sundaystanley56@gmail.com>
2022-12-06 06:06:46 +0100

    Add react-queries and fetched API

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,22 +1,31 @@
-import React from "react";
-import { useQuery } from "react-query";
+import React from 'react'
+import { useQuery } from 'react-query'
+import Planet from './Planet'
 
-const Planets =()=>{
-  const fetchPlanets = async ()=>{
+const Planets = () => {
+  const fetchPlanets = async () => {
     const res = await fetch('https://swapi.dev/api/planets/')
     return res.json()
   }
-  const {data, isLoading, error, status} = useQuery('planets', fetchPlanets)
-  console.log(data);
-  console.log(isLoading);
-  console.log(error);
-  console.log(status);
+  const { data, isLoading, status } = useQuery('planets', fetchPlanets)
+  console.log(data)
+  console.log(isLoading)
+  console.log(status)
 
-return (
-  <div>
-    <h2>planets</h2>
-  </div>
-)
+  return (
+    <div>
+      <h2>planets</h2>
+      {isLoading ? <p>is Loading. . .</p> : ''}
+      {status === 'error' && <div>Error Fetching Data</div>}
+      {status === 'success' && (
+        <div>
+          {data.results.map((planet) => (
+            <Planet key={planet.name} planet={planet} />
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Planets
