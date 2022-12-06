c1bf8bcd5ebf22456620ff03db3e98ec57586c8b
devransom-github <sundaystanley56@gmail.com>
2022-12-06 08:08:38 +0100

    Add pagination to the planet page

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Navbar from './components/Navbar'
 import People from './components/People'
 import Planets from './components/Planets'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 function App() {
   const [page, setPage] = useState('planet')
   const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
-    <div className='App'>
-      <h1>Star wars info</h1>
-      <Navbar setPage={setPage}/>
-      <div className='content'>
-        {page ==='planets' ? <Planets/> : <People/>}
+      <div className='App'>
+        <h1>Star wars info</h1>
+        <Navbar setPage={setPage} />
+        <div className='content'>
+          {page === 'planets' ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -1,28 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from 'react-query'
 import Planet from './Planet'
 
 const Planets = () => {
-  const fetchPlanets = async () => {
-    const res = await fetch('https://swapi.dev/api/planets/')
+  const [page, setPage] = useState(1)
+  const fetchPlanets = async (page) => {
+    const res = await fetch(`https://swapi.dev/api/planets/?page=${page}`)
     return res.json()
   }
-  const { data, isLoading, status } = useQuery('planets', fetchPlanets)
-  console.log(data)
-  console.log(isLoading)
-  console.log(status)
+  const { data, status } = useQuery({
+    queryKey: ['projects', page],
+    queryFn: () => fetchPlanets(page),
+    keepPreviousData: true,
+  })
 
   return (
     <div>
       <h2>planets</h2>
-      {isLoading ? <p>is Loading. . .</p> : ''}
+      {status === 'loading' ? <p>is Loading. . .</p> : ''}
       {status === 'error' && <div>Error Fetching Data</div>}
       {status === 'success' && (
-        <div>
-          {data.results.map((planet) => (
-            <Planet key={planet.name} planet={planet} />
-          ))}
-        </div>
+        <>
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous page
+          </button>
+          <span>{page}</span>
+          <button
+            onClick={() => setPage((old) => (!data.next ? old : old + 1))}
+            disabled={!data.next}
+          >
+            Next page
+          </button>
+          <div>
+            {data.results.map((planet) => (
+              <Planet key={planet.name} planet={planet} />
+            ))}
+          </div>
+        </>
       )}
     </div>
   )
